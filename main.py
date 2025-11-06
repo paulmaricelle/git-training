c18a0c2b359b831777c46c2e2ddf769e47dc1ba3
Paul Maricelle <paul.maricelle@etu.minesparis.psl.eu>
2025-11-06 11:18:03 +0100

    debut

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame as py
+
+def Car():
+    def __init__(self, x, y):
+        self.vx, self.vy = 0, 0
+        self.x, self.y = x, y
+
+    def update(self, dt):
+        self.x += self.vx*dt
+        self.y += self.vy*dt
